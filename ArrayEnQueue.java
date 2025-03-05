a50167cf41afb058c42985fb906e6e55adc094d5
23CSADEEPAKB <139941497+23CSADEEPAKB@users.noreply.github.com>
2025-03-05 10:20:37 +0000

    submit

--- ArrayEnQueue.java
+++ ArrayEnQueue.java
@@ -10,48 +10,63 @@ queue = new int[capacity];
 }
 public int size()
 {
-    return (rear+1);
+ return (rear+1);
 }
 public void enQueue(int data)
 {
-
+if (isFull())
+{
+System.out.println("Queue is Overflow. Not possible to insert in Full Queue");
+display();
+}
+else
+{
+queue[++rear] = data;
+System.out.println("Element is inserted");
+}
 // Type your code here
-
 }
 public boolean isFull()
 {
-
 // Type your code here
+return (size()==capacity);
 }
 public boolean isEmpty()
 {
-
-// Type your code here
+ return (front==rear);
 }
-
-
 public void display()
 {
-    if(isEmpty())
+ if(isEmpty())
 System.out.println("Queue is Underflow. No elements to display in Empty Queue");
-    else
-    {
+ else
+ {
 System.out.println("Queue Elements");
 for(int i=front+1;i<=rear;i++)
 System.out.print(queue[i]+"\t");
 System.out.print("\n");
-    }
+ }
 }
 public static void main(String[] args)
 {
-    int n;
-     Scanner sc=new Scanner(System.in);
+ int n;
+ Scanner sc=new Scanner(System.in);
 int data;
-    char ch;
-     System.out.println("EnQueue Using Array");
-    System.out.println("Enter the number of elements to be inserted:");
-
+ char ch;
+ System.out.println("EnQueue Using Array");
+ System.out.println("Enter the number of elements to be inserted:");
+ n=sc.nextInt();
+ ArrayEnQueue q=new ArrayEnQueue(n);
+ do
+ {
+System.out.println("Enter the element to insert:");
+ data=sc.nextInt();
+q.enQueue(data);
+System.out.println("Do you want to insert(y/n):");
+ch=sc.next().charAt(0);
+}
+while(ch!='n');
+q.display();
+}
 // Type your code here
-
-
-} 
+}
